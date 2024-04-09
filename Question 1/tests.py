ec1254b402b2499111aad173bbc253a8e9480da0
sonny144 <marknyambe14@gmail.com>
2024-04-10 00:11:22 +0200

    test change

--- Question 1/tests.py
+++ Question 1/tests.py
@@ -1,18 +1,26 @@
 from environment import Environment
 from visualization import Visualization
 
-# Test environment and A* search
-env = Environment(6, 6)
-env.add_obstacle(2, 2)
+# Create an instance of the Environment class
+env = Environment(30, 30)
+env.add_obstacle(4, 0) 
+env.add_obstacle(20,1)# Add obstacle 
 
-
+# Define the starting and target positions
 start_pos = (0, 0)
-target_pos = (3, 4)
+target_pos = (28, 2)
+
+# Perform A* search to find the optimal path
 path = env.astar_search(start_pos, target_pos)
 
 # Visualization
 visualization = Visualization(env)
-visualization.plot_environment()
-visualization.plot_path(path)
-visualization.plot_robot(start_pos)
-visualization.show()
+visualization.plot_environment()  # Plot the environment
+
+if path is not None:
+    visualization.plot_path(path)  # Plot the optimal path
+else:
+    print("No valid path found from the starting position to the target position.")
+
+visualization.plot_robot(start_pos)  # Plot the robot's initial position
+visualization.show()  # Display the visualization
